fb66e83730137ee3c81638f34480f317569014f4
Awhn <69659322+Awhn@users.noreply.github.com>
2024-07-04 10:05:33 +0900

    Revert "feat : move to new repo (#135)"
    This reverts commit 6cf7700bb4811c221d1e078a62828c547f6ed066.

--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -1,26 +1,11 @@
 package gift;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-
 @SpringBootApplication
 public class Application {
-    @Autowired
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        final Connection connection = getConnection();
-        connection.close();
-    }
-
-    public static Connection getConnection() throws Exception {
-        var url = "jdbc:h2:mem:test";
-        var user = "sa";
-        var password = "";
-        return DriverManager.getConnection(url,user,password);
     }
 }
